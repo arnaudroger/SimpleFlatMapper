0cbe5d7bab339a73f01060cfce5e4b8e4a3e9af6
Arnaud Roger <arnaud.roger@gmail.com>
2014-07-29 21:23:19 +0100

    add fix heap for benchmark

--- src/test/scripts/reference.sh
+++ src/test/scripts/reference.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp target/classes:target/test-classes/:$HOME/.m2/repository/org/hsqldb/hsqldb/2.3.2/hsqldb-2.3.2.jar:$HOME/.m2/repository/junit/junit/4.11/junit-4.11.jar org.sfm.benchmark.ReferenceBenchmark
+java -Xms1g -cp target/classes:target/test-classes/:$HOME/.m2/repository/org/hsqldb/hsqldb/2.3.2/hsqldb-2.3.2.jar:$HOME/.m2/repository/junit/junit/4.11/junit-4.11.jar org.sfm.benchmark.ReferenceBenchmark

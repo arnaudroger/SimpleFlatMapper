f1e61473f4c309a1f1a5d2ea5fad05edd4a742f5
Arnaud Roger <arnaud.roger@gmail.com>
2014-08-10 21:09:56 +0100

    change all script to use all class

--- src/test/scripts/all.sh
+++ src/test/scripts/all.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo $DIR
-ROOT=$DIR/../../..
 
-OUTDIR=$ROOT/target/perf/benchmarks
-echo $OUTDIR
-mkdir -p $OUTDIR
-$DIR/dynamicJdbcMapperForEach.sh > $OUTDIR/dynamicJdbcMapperForEach.txt
-$DIR/dynamicJdbcMapperMap.sh > $OUTDIR/dynamicJdbcMapperMap.txt
-$DIR/hibernateStatefull.sh > $OUTDIR/hibernateStatefull.txt
-$DIR/hibernateStatefullWithCache.sh > $OUTDIR/hibernateStatefullWithCache.txt
-$DIR/hibernateStateless.sh > $OUTDIR/hibernateStateless.txt
-$DIR/mybatis.sh > $OUTDIR/mybatis.txt
-$DIR/reference.sh > $OUTDIR/reference.txt
-$DIR/staticJdbcMapperMap.sh > $OUTDIR/staticJdbcMapperMap.txt
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+TARGET=$DIR/../../../target
+
+java -Xms2g -Xmx2g -cp $TARGET/classes:$TARGET/test-classes/:\
+$HOME/.m2/repository/org/ow2/asm/asm/5.0.3/asm-5.0.3.jar:\
+$HOME/.m2/repository/org/hsqldb/hsqldb/2.3.2/hsqldb-2.3.2.jar:\
+$HOME/.m2/repository/junit/junit/4.11/junit-4.11.jar:\
+$HOME/.m2/repository/org/hibernate/hibernate-core/4.3.6.Final/hibernate-core-4.3.6.Final.jar:\
+$HOME/.m2/repository/org/hibernate/hibernate-ehcache/4.3.6.Final/hibernate-ehcache-4.3.6.Final.jar:\
+$HOME/.m2/repository/net/sf/ehcache/ehcache-core/2.4.3/ehcache-core-2.4.3.jar:\
+$HOME/.m2/repository/dom4j/dom4j/1.6.1/dom4j-1.6.1.jar:\
+$HOME/.m2/repository/org/hibernate/common/hibernate-commons-annotations/4.0.5.Final/hibernate-commons-annotations-4.0.5.Final.jar:\
+$HOME/.m2/repository/org/jboss/logging/jboss-logging/3.1.3.GA/jboss-logging-3.1.3.GA.jar:\
+$HOME/.m2/repository/org/jboss/spec/javax/transaction/jboss-transaction-api_1.2_spec/1.0.0.Final/jboss-transaction-api_1.2_spec-1.0.0.Final.jar:\
+$HOME/.m2/repository/org/hibernate/javax/persistence/hibernate-jpa-2.1-api/1.0.0.Final/hibernate-jpa-2.1-api-1.0.0.Final.jar:\
+$HOME/.m2/repository/org/javassist/javassist/3.18.1-GA/javassist-3.18.1-GA.jar:\
+$HOME/.m2/repository/antlr/antlr/2.7.7/antlr-2.7.7.jar:\
+$HOME/.m2/repository/org/mybatis/mybatis/3.2.7/mybatis-3.2.7.jar \
+org.sfm.benchmark.AllBenchmark

8fbb273f362094bb6c0ca6b642f4579804501c37
Arnaud Roger <arnaud.roger@gmail.com>
2014-08-10 21:57:03 +0100

    fix static benchmark for small object

--- src/test/java/org/sfm/benchmark/AllBenchmark.java
+++ src/test/java/org/sfm/benchmark/AllBenchmark.java
@@ -15,27 +15,43 @@ public class AllBenchmark {
 		Connection conn = DbHelper.benchmarkDb();
 		Class<SmallBenchmarkObject> target = SmallBenchmarkObject.class;
 		
-		runBenchmark(conn, target, PureJdbcBenchmark.class);
-		runBenchmark(conn, target, StaticJdbcMapperBenchmark.class);
-		runBenchmark(conn, target, DynamicJdbcMapperForEachBenchmark.class);
-		runBenchmark(conn, target, HibernateStatefullBenchmark.class);
-		runBenchmark(conn, target, MyBatisBenchmark.class);
+		int[] limits = new int[] { 1000, 10000, 100000, -1, 100, 10, 1}; 
+		for(int i = 0; i < limits.length; i++) {
+			int limit = limits[i];
+			runBenchmark(conn, target, PureJdbcBenchmark.class, limit);
+			runBenchmark(conn, target, StaticJdbcMapperBenchmark.class, limit);
+			runBenchmark(conn, target, DynamicJdbcMapperForEachBenchmark.class, limit);
+			runBenchmark(conn, target, HibernateStatefullBenchmark.class, limit);
+			runBenchmark(conn, target, MyBatisBenchmark.class, limit);
+		}
 
 	}
 
 	private static void runBenchmark(Connection conn,
 			Class<SmallBenchmarkObject> target,
-			Class<? extends QueryExecutor> benchmark) throws Exception {
+			Class<? extends QueryExecutor> benchmark, int limit) throws Exception {
 		
 		Constructor<? extends QueryExecutor> c = benchmark.getDeclaredConstructor(Connection.class, Class.class);
 		
-		
 		QueryExecutor qe = c.newInstance(conn, target);
-		
-		new BenchmarkRunner(-1, qe).run(new SysOutBenchmarkListener(benchmark, "1MQuery"));
-		new BenchmarkRunner(1, qe).run(new SysOutBenchmarkListener(benchmark, "1Query"));
-		new BenchmarkRunner(10, qe).run(new SysOutBenchmarkListener(benchmark, "10Query"));
-		new BenchmarkRunner(1000, qe).run(new SysOutBenchmarkListener(benchmark, "1KQuery"));
+		String name ;
+		switch (limit) {
+		case 1000:
+			name = "1K";
+			break;
+		case 10000:
+			name = "10K";
+			break;
+		case 100000:
+			name = "100K";
+			break;
+		case 1000000:
+			name = "1M";
+			break;
+		default:
+			name = Integer.toString(limit);
+		}
+		new BenchmarkRunner(limit, qe).run(new SysOutBenchmarkListener(benchmark, name));
 		
 	}
 }
--- src/test/java/org/sfm/benchmark/sfm/StaticJdbcMapperBenchmark.java
+++ src/test/java/org/sfm/benchmark/sfm/StaticJdbcMapperBenchmark.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 import org.sfm.beans.DbObject;
+import org.sfm.beans.SmallBenchmarkObject;
 import org.sfm.benchmark.BenchmarkRunner;
 import org.sfm.benchmark.SysOutBenchmarkListener;
 import org.sfm.jdbc.DbHelper;
@@ -19,19 +20,33 @@ public class StaticJdbcMapperBenchmark<T> extends ForEachMapperQueryExecutor<T> 
 	@SuppressWarnings("unchecked")
 	private static <T> DelegateJdbcMapper<T> newMapper(Class<T> target) {
 		if (target.equals(DbObject.class)) {
-		return (DelegateJdbcMapper<T>) 
-				new DelegateJdbcMapper<DbObject>(
-						new ResultSetMapperBuilder<DbObject>(DbObject.class)
-							.addIndexedColumn("id")
-							.addIndexedColumn("name")
-							.addIndexedColumn("email")
-							.addIndexedColumn("creation_time").mapper(), 
-				new Instantiator<DbObject>() {
-					@Override
-					public DbObject newInstance() throws Exception {
-						return new DbObject();
-					}
-				});
+			return (DelegateJdbcMapper<T>) 
+					new DelegateJdbcMapper<DbObject>(
+							new ResultSetMapperBuilder<DbObject>(DbObject.class)
+								.addIndexedColumn("id")
+								.addIndexedColumn("name")
+								.addIndexedColumn("email")
+								.addIndexedColumn("creation_time").mapper(), 
+					new Instantiator<DbObject>() {
+						@Override
+						public DbObject newInstance() throws Exception {
+							return new DbObject();
+						}
+					});
+		} else if (target.equals(SmallBenchmarkObject.class)) {
+			return (DelegateJdbcMapper<T>) 
+					new DelegateJdbcMapper<SmallBenchmarkObject>(
+							new ResultSetMapperBuilder<SmallBenchmarkObject>(SmallBenchmarkObject.class)
+								.addIndexedColumn("id")
+								.addIndexedColumn("name")
+								.addIndexedColumn("email")
+								.addIndexedColumn("year_started").mapper(), 
+					new Instantiator<SmallBenchmarkObject>() {
+						@Override
+						public SmallBenchmarkObject newInstance() throws Exception {
+							return new SmallBenchmarkObject();
+						}
+					});
 		} else {
 			throw new UnsupportedOperationException(target.getName());
 		}

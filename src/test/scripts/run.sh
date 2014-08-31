20210c3099321b525bc58b2a3d6caef22ea80a85
Arnaud Roger <arnaud.roger@gmail.com>
2014-08-31 18:30:54 +0100

    change script to add date to data file

--- src/test/scripts/run.sh
+++ src/test/scripts/run.sh
@@ -7,11 +7,16 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 BENCHMARK=$DIR/../../../src/test/benchmarks
 SCRIPTS=$DIR/../../../src/test/scripts
 
-cpustable
-$SCRIPTS/allBenchmarks.sh mock 1,10,100,1000 1000000 > $BENCHMARK/data/mock.csv
-$SCRIPTS/allBenchmarks.sh hsqldb 1,10,100,1000 500000 > $BENCHMARK/data/hsqldb.csv
-$SCRIPTS/allBenchmarks.sh mysql 1,10,100,1000 100000 > $BENCHMARK/data/mysql.csv
-cpufull
+DATE=`date +%Y%m%d_%H%M%S`
+
+$SCRIPTS/cpuPreBenchmark.sh
+$SCRIPTS/allBenchmarks.sh mock 1,10,100,1000 1000000 > $BENCHMARK/data/mock/mock_$DATE.csv
+$SCRIPTS/allBenchmarks.sh hsqldb 1,10,100,1000 500000 > $BENCHMARK/data/hsqldb/hsqldb_$DATE.csv
+$SCRIPTS/allBenchmarks.sh mysql 1,10,100,1000 100000 > $BENCHMARK/data/mysql/mysql_$DATE.csv
+$SCRIPTS/cpuPostBenchmark.sh
+
+git add $BENCHMARK/data/mock/mock_$DATE.csv $BENCHMARK/data/hsqldb/hsqldb_$DATE.csv $BENCHMARK/data/mysql/mysql_$DATE.csv
+git commit -m "$DATE benchmark"
 
 
 

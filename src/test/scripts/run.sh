3f70bf2a73f3a6b78f9418ad68276026a3f963b9
Arnaud Roger <arnaud.roger@gmail.com>
2014-08-31 18:06:01 +0100

    fix benchmark script

--- src/test/scripts/run.sh
+++ src/test/scripts/run.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-fn_exists()
-{
-    type $1 | grep -q 'shell function'
-}
+
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 BENCHMARK=$DIR/../../../src/test/benchmarks
 SCRIPTS=$DIR/../../../src/test/scripts
 
-fn_exists cpustable && cpustable
-$SCRIPTS/all.sh mock 1,10,100,1000 1000000 > $BENCHMARK/data/mock.csv
-$SCRIPTS/all.sh hsqldb 1,10,100,1000 500000 > $BENCHMARK/data/hsqldb.csv
-$SCRIPTS/all.sh mysql 1,10,100,1000 100000 > $BENCHMARK/data/mysql.csv
-fn_exists cpufull && cpufull
+cpustable
+$SCRIPTS/allBenchmarks.sh mock 1,10,100,1000 1000000 > $BENCHMARK/data/mock.csv
+$SCRIPTS/allBenchmarks.sh hsqldb 1,10,100,1000 500000 > $BENCHMARK/data/hsqldb.csv
+$SCRIPTS/allBenchmarks.sh mysql 1,10,100,1000 100000 > $BENCHMARK/data/mysql.csv
+cpufull
 
 
 

035a551d4a2f8beedb10c1c37e8d2e024b3f38c6
Arnaud Roger <arnaud.roger@gmail.com>
2014-08-31 16:33:15 +0100

    rename benchmark xls

--- src/test/java/org/sfm/benchmark/sql2o/Sql2OBenchmark.java
+++ src/test/java/org/sfm/benchmark/sql2o/Sql2OBenchmark.java
@@ -4,12 +4,15 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.sfm.benchmark.ForEachListener;
 import org.sfm.benchmark.JDBCHelper;
 import org.sfm.benchmark.QueryExecutor;
+import org.sfm.benchmark.RunBenchmark;
 import org.sfm.benchmark.SingleConnectionDataSource;
+import org.sfm.jdbc.DbHelper;
 import org.sql2o.Sql2o;
 
 public class Sql2OBenchmark implements QueryExecutor {
@@ -41,5 +44,10 @@ public class Sql2OBenchmark implements QueryExecutor {
 			}
 		}
 	}
+	
+	public static void main(String[] args) throws SQLException, Exception {
+		RunBenchmark.runBenchmark(DbHelper.getConnection(args), Sql2OBenchmark.class);
+	}
+
 	
 }

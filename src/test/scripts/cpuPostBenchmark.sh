556152a0525a0c1c1ff4a751f6c0c5642d1410ac
Arnaud Roger <arnaud.roger@gmail.com>
2014-08-31 18:34:23 +0100

    fix cpu script

--- src/test/scripts/cpuPostBenchmark.sh
+++ src/test/scripts/cpuPostBenchmark.sh
@@ -1,7 +1,3 @@
-cpufull is a function
-cpufull () 
-{ 
     echo "ondemand" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor;
     echo "800000" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq;
     echo "2668000" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq
-}
--- src/test/scripts/cpuPreBenchmark.sh
+++ src/test/scripts/cpuPreBenchmark.sh
@@ -1,7 +1,3 @@
-cpustable is a function
-cpustable () 
-{ 
     echo "performance" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor;
     echo "2000000" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq;
     echo "2000000" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq
-}

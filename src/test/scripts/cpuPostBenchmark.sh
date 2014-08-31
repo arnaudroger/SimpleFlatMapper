5747e3ae5b671a8a0b5b97ff35d1f7e250ca8ea2
Arnaud Roger <arnaud.roger@gmail.com>
2014-08-31 18:19:41 +0100

    add cpu frequency script

---
+++ src/test/scripts/cpuPostBenchmark.sh
@@ -0,0 +1,7 @@
+cpufull is a function
+cpufull () 
+{ 
+    echo "ondemand" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor;
+    echo "800000" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq;
+    echo "2668000" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq
+}
---
+++ src/test/scripts/cpuPreBenchmark.sh
@@ -0,0 +1,7 @@
+cpustable is a function
+cpustable () 
+{ 
+    echo "performance" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor;
+    echo "2000000" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_min_freq;
+    echo "2000000" | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_max_freq
+}

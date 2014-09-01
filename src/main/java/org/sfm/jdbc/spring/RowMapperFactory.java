ce1da7c52e57f7393b1788e5524595cbae6e91ac
Arnaud Roger <arnaud.roger@gmail.com>
2014-09-01 17:54:42 +0100

    add bundle metadata generate and spring wrapper for RowMapper

---
+++ src/main/java/org/sfm/jdbc/spring/RowMapperFactory.java
@@ -0,0 +1,39 @@
+package org.sfm.jdbc.spring;
+
+import java.io.IOException;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
+import org.sfm.jdbc.JdbcMapperFactory;
+import org.sfm.map.FieldMapperErrorHandler;
+import org.sfm.map.MapperBuilderErrorHandler;
+import org.springframework.jdbc.core.RowMapper;
+
+public final class RowMapperFactory {
+	private final JdbcMapperFactory jdbcMapperFactory = new JdbcMapperFactory();
+
+	public RowMapperFactory fieldMapperErrorHandler(FieldMapperErrorHandler fieldMapperErrorHandler) {
+		jdbcMapperFactory.fieldMapperErrorHandler(fieldMapperErrorHandler);
+		return this;
+	}
+
+	public RowMapperFactory mapperBuilderErrorHandler(
+			MapperBuilderErrorHandler mapperBuilderErrorHandler) {
+		jdbcMapperFactory.mapperBuilderErrorHandler(mapperBuilderErrorHandler);
+		return this;
+	}
+
+	public RowMapperFactory useAsm(boolean useAsm) {
+		jdbcMapperFactory.useAsm(useAsm);
+		return this;
+	}
+
+	public <T> RowMapper<T> newMapper(Class<T> target,	ResultSetMetaData metaData) throws SQLException,
+			NoSuchMethodException, SecurityException, IOException {
+		return new RowMapperDelegate<>(jdbcMapperFactory.newMapper(target, metaData));
+	}
+	public <T> RowMapper<T> newMapper(Class<T> target)
+			throws NoSuchMethodException, SecurityException {
+		return new RowMapperDelegate<>(jdbcMapperFactory.newMapper(target));
+	}
+}

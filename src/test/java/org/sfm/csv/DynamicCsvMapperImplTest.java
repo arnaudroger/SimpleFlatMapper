656c1f3ef37fdc5ec437f33db8ac8f0c3e3015a5
Arnaud Roger <arnaud.roger@gmail.com>
2014-09-25 22:05:50 +0100

    list test

--- src/test/java/org/sfm/csv/DynamicCsvMapperImplTest.java
+++ src/test/java/org/sfm/csv/DynamicCsvMapperImplTest.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 import org.junit.Test;
 import org.sfm.beans.DbFinalObject;
+import org.sfm.beans.DbListObject;
 import org.sfm.beans.DbObject;
 import org.sfm.beans.DbPartialFinalObject;
 import org.sfm.jdbc.DbHelper;
@@ -71,5 +72,19 @@ public class DynamicCsvMapperImplTest {
 		list = mapper.forEach(dbObjectCsvReader(), new ListHandler<DbPartialFinalObject>()).getList();
 		assertEquals(1, list.size());
 		DbHelper.assertDbObjectMapping(list.get(0));
+	}
+	
+	private static final String CSV_LIST = "id,objects_0_id,objects_0_name,objects_0_email,objects_0_creationTime,objects_0_typeOrdinal,objects_0_typeName\n"
+			+ "1,1,name 1,name1@mail.com,2014-03-04 11:10:03,2,type4";
+	@Test
+	public void testDbListObject() throws Exception {
+		
+		CsvMapper<DbListObject> mapper = CsvMapperFactory.newInstance().newMapper(DbListObject.class);
+		
+		
+		List<DbListObject> list = mapper.forEach(new StringReader(CSV_LIST), new ListHandler<DbListObject>()).getList();
+		assertEquals(1, list.size());
+		DbHelper.assertDbObjectMapping(list.get(0).getObjects().get(0));
+
 	}
 }

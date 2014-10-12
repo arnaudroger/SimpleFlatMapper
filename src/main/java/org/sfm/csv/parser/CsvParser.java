8ee945cae637adb58252553ca44b7bc2bab7ef3d
Arnaud Roger <arnaud.roger@gmail.com>
2014-10-12 16:59:50 +0100

    Change default size

--- src/main/java/org/sfm/csv/parser/CsvParser.java
+++ src/main/java/org/sfm/csv/parser/CsvParser.java
@@ -6,6 +6,11 @@ import java.io.Reader;
 import org.sfm.utils.RowHandler;
 
 public final class CsvParser {
+	public static int _4K = 1 << 12;
+	public static int _8K = 1 << 13;
+	public static int _16K = 1 << 14;
+	public static int _32K = 1 << 15;
+	public static int _64K = 1 << 16;
 	
 	private final int bufferSize;
 	
@@ -14,7 +19,7 @@ public final class CsvParser {
 	}
 	
 	public CsvParser() {
-		this(1<<16);
+		this(_8K);
 	}
 		
 	public <CH extends CharsCellHandler> CH parse(final Reader r, final CH handler) throws IOException {

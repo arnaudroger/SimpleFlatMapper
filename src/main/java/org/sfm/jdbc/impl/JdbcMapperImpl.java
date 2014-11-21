a606110556de0638044d17479fc9a1f785f69c45
Arnaud Roger <arnaud.roger@gmail.com>
2014-11-21 21:25:03 +0000

    implement own spliterator for jdbc

--- src/main/java/org/sfm/jdbc/impl/JdbcMapperImpl.java
+++ src/main/java/org/sfm/jdbc/impl/JdbcMapperImpl.java
@@ -1,15 +1,19 @@
 package org.sfm.jdbc.impl;
 
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Iterator;
 //IFJAVA8_START
+import java.util.function.Consumer;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 import java.util.Spliterator;
 import java.util.Spliterators;
 //IFJAVA8_END
 
+import org.sfm.csv.parser.CellConsumer;
+import org.sfm.csv.parser.CsvReader;
 import org.sfm.jdbc.JdbcMapper;
 import org.sfm.map.MappingException;
 import org.sfm.map.RowHandlerErrorHandler;
@@ -51,8 +55,58 @@ public final class JdbcMapperImpl<T> extends MapperImpl<ResultSet, T> implements
 	//IFJAVA8_START
 	@Override
 	public Stream<T> stream(ResultSet rs) throws SQLException, MappingException {
-		Spliterator<T> spliterator = Spliterators.spliteratorUnknownSize(iterate(rs), Spliterator.DISTINCT | Spliterator.ORDERED);
-		return StreamSupport.stream(spliterator, false);
+		return StreamSupport.stream(new JdbcSpliterator(rs), false);
 	}
+
+	public class JdbcSpliterator implements Spliterator<T> {
+		private final ResultSet resultSet;
+
+		public JdbcSpliterator(ResultSet resultSet) {
+			this.resultSet = resultSet;
+		}
+
+		@Override
+		public boolean tryAdvance(Consumer<? super T> action) {
+			try {
+				if (resultSet.next()) {
+                    action.accept(map(resultSet));
+                    return true;
+                }
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
+			}
+			return false;
+		}
+
+		@Override
+		public void forEachRemaining(Consumer<? super T> action) {
+			try {
+				forEach(resultSet, new RowHandler<T>() {
+                    @Override
+                    public void handle(T t) throws Exception {
+                        action.accept(t);
+                    }
+                });
+			} catch (SQLException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		@Override
+		public Spliterator<T> trySplit() {
+			return null;
+		}
+
+		@Override
+		public long estimateSize() {
+			return Long.MAX_VALUE;
+		}
+
+		@Override
+		public int characteristics() {
+			return Spliterator.ORDERED | Spliterator.NONNULL;
+		}
+	}
+
 	//IFJAVA8_END
 }

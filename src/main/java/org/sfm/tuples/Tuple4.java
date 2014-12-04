10b21fae8582b3bec40ff527ca339ec67be43e6b
Arnaud Roger <arnaud.roger@gmail.com>
2014-12-04 23:44:24 +0000

    add more tuples

--- src/main/java/org/sfm/tuples/Tuple4.java
+++ src/main/java/org/sfm/tuples/Tuple4.java
@@ -3,16 +3,18 @@ package org.sfm.tuples;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
-public class Tuple3<T1, T2, T3> {
+public class Tuple4<T1, T2, T3, T4> {
 
     private final T1 element1;
     private final T2 element2;
     private final T3 element3;
+    private final T4 element4;
 
-    public Tuple3(T1 element1, T2 element2, T3 element3) {
+    public Tuple4(T1 element1, T2 element2, T3 element3, T4 element4) {
         this.element1 = element1;
         this.element2 = element2;
         this.element3 = element3;
+        this.element4 = element4;
     }
 
     public T1 getElement1() {
@@ -27,11 +29,15 @@ public class Tuple3<T1, T2, T3> {
         return element3;
     }
 
-    public static <T1, T2, T3> ParameterizedType typeDef(final Class<T1> c1, final Class<T2> c2, final Class<T3> c3) {
+    public T4 getElement4() {
+        return element4;
+    }
+
+    public static <T1, T2, T3, T4> ParameterizedType typeDef(final Class<T1> c1, final Class<T2> c2, final Class<T3> c3, final Class<T4> c4) {
         return new ParameterizedType() {
             @Override
             public Type[] getActualTypeArguments() {
-                return new Type[] { c1, c2, c3 };
+                return new Type[] { c1, c2, c3, c4 };
             }
 
             @Override
--- src/main/java/org/sfm/tuples/Tuple5.java
+++ src/main/java/org/sfm/tuples/Tuple5.java
@@ -3,16 +3,20 @@ package org.sfm.tuples;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 
-public class Tuple3<T1, T2, T3> {
+public class Tuple5<T1, T2, T3, T4, T5> {
 
     private final T1 element1;
     private final T2 element2;
     private final T3 element3;
+    private final T4 element4;
+    private final T5 element5;
 
-    public Tuple3(T1 element1, T2 element2, T3 element3) {
+    public Tuple5(T1 element1, T2 element2, T3 element3, T4 element4, T5 element5) {
         this.element1 = element1;
         this.element2 = element2;
         this.element3 = element3;
+        this.element4 = element4;
+        this.element5 = element5;
     }
 
     public T1 getElement1() {
@@ -27,11 +31,21 @@ public class Tuple3<T1, T2, T3> {
         return element3;
     }
 
-    public static <T1, T2, T3> ParameterizedType typeDef(final Class<T1> c1, final Class<T2> c2, final Class<T3> c3) {
+    public T4 getElement4() {
+        return element4;
+    }
+
+    public T5 getElement5() {
+        return element5;
+    }
+
+    public static <T1, T2, T3, T4, T5> ParameterizedType typeDef(final Class<T1> c1, final Class<T2> c2,
+                                                             final Class<T3> c3, final Class<T4> c4,
+                                                             final Class<T5> c5) {
         return new ParameterizedType() {
             @Override
             public Type[] getActualTypeArguments() {
-                return new Type[] { c1, c2, c3 };
+                return new Type[] { c1, c2, c3, c4, c5 };
             }
 
             @Override

e1b979d4b9b31a332abbe765186cfef50db4913d
Arnaud Roger <arnaud.roger@gmail.com>
2015-01-12 22:55:10 +0000

    remove unused code

--- src/main/java/org/sfm/map/ColumnDefinition.java
+++ src/main/java/org/sfm/map/ColumnDefinition.java
@@ -5,13 +5,4 @@ import org.sfm.map.impl.FieldKey;
 
 public abstract class ColumnDefinition<K extends FieldKey<K>> {
     public abstract K rename(K key);
-
-    public static <K extends FieldKey<K>> ColumnDefinition<K> identity() {
-        return new ColumnDefinition<K>() {
-            @Override
-            public K rename(K key) {
-                return key;
-            }
-        };
-    }
 }

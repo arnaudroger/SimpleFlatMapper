b899b8bc5b281191f18071a7ff435c73b5b7edbd
Arnaud Roger <arnaud.roger@gmail.com>
2015-03-23 21:35:32 +0000

    fix java6 compile

--- src/main/java/org/sfm/reflect/asm/AsmFactory.java
+++ src/main/java/org/sfm/reflect/asm/AsmFactory.java
@@ -172,7 +172,7 @@ public class AsmFactory {
         final String className = generateClassCsvMapperCellHandler(target, delayedCellSetterFactories, setters);
         final String factoryName = className + "Factory";
         final byte[] bytes = CsvMapperCellHandlerBuilder.<T>createTargetSetterClass(className, delayedCellSetterFactories, setters, target);
-        final byte[] bytesFactory = CsvMapperCellHandlerBuilder.<T>createTargetSetterFactory(factoryName, className, target);
+        final byte[] bytesFactory = CsvMapperCellHandlerBuilder.createTargetSetterFactory(factoryName, className, target);
         final Class<?> type = createClass(className, bytes, target.getClass().getClassLoader());
         final Class<?> typeFactory = createClass(factoryName, bytesFactory, target.getClass().getClassLoader());
 

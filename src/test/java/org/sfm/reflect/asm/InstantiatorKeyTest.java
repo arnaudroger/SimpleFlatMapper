9613682203f9560e8c62574babdb0fd35d66d1ff
Arnaud Roger <arnaud.roger@gmail.com>
2015-05-25 11:33:54 +0100

    add test for key equals

--- src/test/java/org/sfm/reflect/asm/InstantiatorKeyTest.java
+++ src/test/java/org/sfm/reflect/asm/InstantiatorKeyTest.java
@@ -1,6 +1,7 @@
 package org.sfm.reflect.asm;
 
 import org.junit.Test;
+import org.sfm.beans.DbFinalObject;
 import org.sfm.beans.DbObject;
 
 import java.io.InputStream;
@@ -25,10 +26,37 @@ public class InstantiatorKeyTest {
     }
 
     @Test
+    public void testEqualsOnSameSourceAndTargetAndInjectParamValues() throws NoSuchMethodException {
+        InstantiatorKey k1 = new InstantiatorKey(DbObject.class.getConstructor(), new String[] { "param"}, ResultSet.class);
+        InstantiatorKey k2 = new InstantiatorKey(DbObject.class.getConstructor(), new String[] { "param"}, ResultSet.class);
+        assertTrue(k1.equals(k2));
+    }
+
+    @Test
     public void testNotEqualsOnDiffSourceAndSameTargetValues() throws NoSuchMethodException {
         InstantiatorKey k1 = new InstantiatorKey(DbObject.class, ResultSet.class);
         InstantiatorKey k2 = new InstantiatorKey(DbObject.class, InputStream.class);
         InstantiatorKey k3 = new InstantiatorKey(DbObject.class, null);
+        assertFalse(k1.equals(k2));
+        assertFalse(k1.equals(k3));
+        assertFalse(k3.equals(k1));
+    }
+
+    @Test
+    public void testNotEqualsOnSameSourceAndTargetAndDiffInjectParamValues() throws NoSuchMethodException {
+        InstantiatorKey k1 = new InstantiatorKey(DbObject.class.getConstructor(), new String[] { "param"}, ResultSet.class);
+        InstantiatorKey k2 = new InstantiatorKey(DbObject.class.getConstructor(), new String[] { "param2"}, ResultSet.class);
+        InstantiatorKey k3 = new InstantiatorKey(DbObject.class.getConstructor(), null, ResultSet.class);
+        assertFalse(k1.equals(k2));
+        assertFalse(k1.equals(k3));
+        assertFalse(k3.equals(k1));
+    }
+
+    @Test
+    public void testNotEqualsDiffConstructor() throws NoSuchMethodException {
+        InstantiatorKey k1 = new InstantiatorKey(DbObject.class.getConstructor(), new String[] { "param"}, ResultSet.class);
+        InstantiatorKey k2 = new InstantiatorKey(DbFinalObject.class.getDeclaredConstructors()[0], new String[] { "param"}, ResultSet.class);
+        InstantiatorKey k3 = new InstantiatorKey(null, new String[] { "param"}, ResultSet.class);
         assertFalse(k1.equals(k2));
         assertFalse(k1.equals(k3));
         assertFalse(k3.equals(k1));

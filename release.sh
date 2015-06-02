dbc84784f7e4aa2c38dff6b8f81ecafcbc6f2bef
Arnaud Roger <arnaud.roger@gmail.com>
2015-06-02 21:38:41 +0100

    fix release

--- release.sh
+++ release.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+function java8 {
+	sudo update-alternatives --set java /usr/lib/jvm/java-8-oracle/jre/bin/java;export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+}
+function java7 {
+	sudo update-alternatives --set java /usr/lib/jvm/java-7-oracle/jre/bin/java;export JAVA_HOME=/usr/lib/jvm/java-7-oracle
+}
+function java6 { 
+	sudo update-alternatives --set java /usr/lib/jvm/java-6-oracle/jre/bin/java;export JAVA_HOME=/usr/lib/jvm/java-6-oracle
+}
 java8
 mvn release:prepare
 cp release.properties tmp/release.properties

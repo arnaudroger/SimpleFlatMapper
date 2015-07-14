25d1e52223ad751d27edb3f7c9fece04a7d0aeea
Arnaud Roger <arnaud.roger@gmail.com>
2015-07-14 19:00:32 +0100

    update buildAll script

--- buildAll.sh
+++ buildAll.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+function java8 {
+	sudo update-alternatives --set java /usr/lib/jvm/java-8-oracle/jre/bin/java;export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+}
+function java7 {
+	sudo update-alternatives --set java /usr/lib/jvm/java-7-oracle/jre/bin/java;export JAVA_HOME=/usr/lib/jvm/java-7-oracle
+}
+function java6 {
+	sudo update-alternatives --set java /usr/lib/jvm/java-6-oracle/jre/bin/java;export JAVA_HOME=/usr/lib/jvm/java-6-oracle
+}
+
 git reset --hard && java8 && mvn clean install  && java7  && mvn clean install && java6 && mvn clean install && git reset --hard 
 

1a9271822c505150e1b1d17e35c9274c6ff3d074
Arnaud Roger <arnaud.roger@gmail.com>
2015-10-05 18:59:56 +0100

    #228 batch test

--- sfm-datastax/src/test/java/org/sfm/datastax/SettableDataMapperTest.java
+++ sfm-datastax/src/test/java/org/sfm/datastax/SettableDataMapperTest.java
@@ -1,11 +1,15 @@
 package org.sfm.datastax;
 
 import com.datastax.driver.core.*;
+import com.datastax.driver.core.querybuilder.Batch;
+import com.datastax.driver.core.querybuilder.QueryBuilder;
 import org.junit.Test;
 import org.sfm.beans.DbObject;
 import org.sfm.map.Mapper;
+import org.sfm.reflect.TypeReference;
 
-import java.util.Date;
+import java.lang.reflect.Type;
+import java.util.*;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -13,7 +17,10 @@ import static org.junit.Assert.assertFalse;
 
 public class SettableDataMapperTest extends AbstractDatastaxTest {
 
-    DbObject dbObject = DbObject.newInstance();
+    public static final String QUERY = "insert into " +
+            "dbobjects(id, name, email, creation_time, type_ordinal, type_name) " +
+            "values(?, ?, ?, ?, ?, ?)";
+    List<DbObject> dbObjects = Arrays.asList(DbObject.newInstance(), DbObject.newInstance());
 
     @Test
     public void testInsertDbObjects() throws Exception {
@@ -27,13 +34,45 @@ public class SettableDataMapperTest extends AbstractDatastaxTest {
 
                 DatastaxBinder<DbObject> datastaxBinder = DatastaxMapperFactory.newInstance().mapFrom(DbObject.class);
 
-                session.execute(datastaxBinder.mapTo(dbObject, preparedStatement));
+                session.execute(datastaxBinder.mapTo(dbObjects.get(0), preparedStatement));
+                checkObjectInserted(session, 0);
 
-                DatastaxMapper<DbObject> dbObjectDatastaxMapper = DatastaxMapperFactory.newInstance().mapTo(DbObject.class);
-                BoundStatement boundStatement = session.prepare("select * from dbobjects where id = ?").bind(dbObject.getId());
-                ResultSet execute = session.execute(boundStatement);
-                DbObject actual = dbObjectDatastaxMapper.iterator(execute).next();
-                assertEquals(dbObject, actual);
+            }
+        });
+    }
+
+    protected void checkObjectInserted(Session session, int i) {
+        DbObject object = dbObjects.get(i);
+        DatastaxMapper<DbObject> dbObjectDatastaxMapper = DatastaxMapperFactory.newInstance().mapTo(DbObject.class);
+        BoundStatement boundStatement = session.prepare("select * from dbobjects where id = ?").bind(object.getId());
+        ResultSet execute = session.execute(boundStatement);
+        DbObject actual = dbObjectDatastaxMapper.iterator(execute).next();
+        assertEquals(object, actual);
+    }
+
+
+    @Test
+    public void testInsertDbObjectsBatch() throws Exception {
+        testInSession(new Callback() {
+            @Override
+            public void call(Session session) throws Exception {
+
+
+                Batch bs = QueryBuilder.batch();
+
+                bs.add(new SimpleStatement(QUERY));
+                bs.add(new SimpleStatement(QUERY));
+
+
+                PreparedStatement preparedStatement = session.prepare(bs);
+
+                DatastaxBinder<List<DbObject>> datastaxBinder = DatastaxMapperFactory.newInstance().mapFrom(new TypeReference<List<DbObject>>() {
+                });
+
+                session.execute(datastaxBinder.mapTo(dbObjects, preparedStatement));
+
+                checkObjectInserted(session, 0);
+                checkObjectInserted(session, 1);
 
             }
         });
@@ -41,6 +80,7 @@ public class SettableDataMapperTest extends AbstractDatastaxTest {
 
     @Override
     protected void tearDown(Session session) {
-        session.execute(session.prepare("delete from dbobjects where id = ?").bind(dbObject.getId()));
+        session.execute(session.prepare("delete from dbobjects where id = ?").bind(dbObjects.get(0).getId()));
+        session.execute(session.prepare("delete from dbobjects where id = ?").bind(dbObjects.get(1).getId()));
     }
 }

f00a89d7f419e4178cd23b9a9a9552d5d2b90345
Arnaud Roger <arnaud.roger@gmail.com>
2016-02-24 12:11:57 +0000

    increase jdbc template coverage

--- sfm-springjdbc/src/test/java/org/sfm/jdbc/spring/JdbcTemplateCrudTest.java
+++ sfm-springjdbc/src/test/java/org/sfm/jdbc/spring/JdbcTemplateCrudTest.java
@@ -4,11 +4,14 @@ import org.junit.Before;
 import org.junit.Test;
 import org.sfm.beans.DbObject;
 import org.sfm.test.jdbc.DbHelper;
+import org.sfm.utils.ListCollectorHandler;
 import org.sfm.utils.RowHandler;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.SingleConnectionDataSource;
 
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
@@ -60,6 +63,47 @@ public class JdbcTemplateCrudTest {
 		assertNull(objectCrud.read(key));
 
 		objectCrud.create(DbObject.newInstance());
+	}
+
+
+	@Test
+	public void testCrudBatch() throws SQLException {
+		JdbcTemplateCrud<DbObject, Long> objectCrud =
+				JdbcTemplateMapperFactory.newInstance()
+						.<DbObject, Long>crud(DbObject.class, Long.class).to(template, "TEST_DB_OBJECT");
+		DbObject object = DbObject.newInstance();
+
+
+		assertNull(objectCrud.read(object.getId()));
+
+		// create
+		final List<DbObject> objects = Arrays.asList(object);
+		Long key =
+				objectCrud.create(objects, new RowHandler<Long>() {
+					Long key;
+					@Override
+					public void handle(Long aLong) throws Exception {
+						key = aLong;
+					}
+				}).key;
+
+		assertNull(key);
+
+
+		List<Long> keys = Arrays.asList(object.getId());
+		// read
+		assertEquals(objects, objectCrud.read(keys, new ListCollectorHandler<DbObject>()).getList());
+
+		object.setName("Udpdated");
+
+		// update
+		objectCrud.update(objects);
+		assertEquals(object, objectCrud.read(object.getId()));
+
+		// delete
+		objectCrud.delete(keys);
+		assertNull(objectCrud.read(key));
+
 	}
 
 }

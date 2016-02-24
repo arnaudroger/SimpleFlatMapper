e2d0288d7ddf28d7aafa3fcb88517d4e1c41575f
Arnaud Roger <arnaud.roger@gmail.com>
2016-02-24 14:24:28 +0000

    increase defaultcrud coverage

--- sfm/src/test/java/org/sfm/jdbc/CrudUpsertTest.java
+++ sfm/src/test/java/org/sfm/jdbc/CrudUpsertTest.java
@@ -1,7 +1,6 @@
 package org.sfm.jdbc;
 
 import org.junit.Test;
-import org.mockito.ArgumentCaptor;
 import org.sfm.beans.DbObject;
 import org.sfm.jdbc.impl.ColumnMeta;
 import org.sfm.jdbc.impl.CrudFactory;
@@ -17,18 +16,18 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 public class CrudUpsertTest {
 
-    Crud<DbObject, Long> objectCrud;
+    Crud<DbObject, Long> postGresqlCrud;
 
     {
         try {
-            objectCrud =
+            postGresqlCrud =
                     CrudFactory.newInstance(
                     ReflectionService.newInstance().getClassMeta(DbObject.class),
                     ReflectionService.newInstance().getClassMeta(Long.class),
@@ -43,6 +42,24 @@ public class CrudUpsertTest {
         }
     }
 
+    Crud<DbObject, Long> hsqlCrud;
+
+    {
+        try {
+            hsqlCrud =
+                    CrudFactory.newInstance(
+                            ReflectionService.newInstance().getClassMeta(DbObject.class),
+                            ReflectionService.newInstance().getClassMeta(Long.class),
+                            new CrudMeta(new DatabaseMeta("HsqlDB", 5, 5), "TEST",
+                                    new ColumnMeta[]{
+                                            new ColumnMeta("id", Types.INTEGER, true, false),
+                                            new ColumnMeta("name", Types.VARCHAR, false, false)
+                                    }),
+                            JdbcMapperFactory.newInstance());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
     @Test
     public void testUpsertDefaultCrud() throws SQLException {
 
@@ -57,7 +74,7 @@ public class CrudUpsertTest {
                         "SET name = EXCLUDED.name", new String[0])).thenReturn(ps);
 
         DbObject o = DbObject.newInstance();
-        objectCrud.createOrUpdate(connection, o);
+        postGresqlCrud.createOrUpdate(connection, o);
 
         verify(ps).setLong(1, o.getId());
         verify(ps).setString(2, o.getName());
@@ -78,7 +95,7 @@ public class CrudUpsertTest {
                         "SET name = EXCLUDED.name")).thenReturn(ps);
 
         List<DbObject> objects = Arrays.asList(DbObject.newInstance(), DbObject.newInstance());
-        objectCrud.createOrUpdate(connection, objects);
+        postGresqlCrud.createOrUpdate(connection, objects);
 
         verify(ps).setLong(1, objects.get(0).getId());
         verify(ps).setString(2, objects.get(0).getName());
@@ -86,5 +103,23 @@ public class CrudUpsertTest {
         verify(ps).setString(4, objects.get(1).getName());
 
         verify(ps).executeUpdate();
+    }
+
+    @Test
+    public void testUpsertHsqlBatch() throws SQLException {
+
+        try {
+            Connection connection = mock(Connection.class);
+            PreparedStatement ps = mock(PreparedStatement.class);
+
+
+            List<DbObject> objects = Arrays.asList(DbObject.newInstance(), DbObject.newInstance());
+            hsqlCrud.createOrUpdate(connection, objects);
+
+            fail();
+        } catch(UnsupportedOperationException e) {
+            // expected
+        }
+
     }
 }

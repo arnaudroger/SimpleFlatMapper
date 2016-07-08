f74e2293158c0a3df48907faab4559717d40ef44
Arnaud Roger <arnaud.roger@gmail.com>
2016-07-08 10:27:18 +0100

    makes cell consumer a functional interface

--- sfm/src/main/java/org/sfm/csv/parser/CellConsumer.java
+++ sfm/src/main/java/org/sfm/csv/parser/CellConsumer.java
@@ -1,7 +1,26 @@
 package org.sfm.csv.parser;
 
+//IFJAVA8_START
+@FunctionalInterface
+//IFJAVA8_END
 public interface CellConsumer {
 	void newCell(char[] chars, int offset, int length);
-	void endOfRow();
-	void end();
+//IFJAVA8_START
+	default
+//IFJAVA8_END
+	void endOfRow()
+//IFJAVA8_START
+	{
+	}
+//IFJAVA8_END
+	;
+//IFJAVA8_START
+	default
+//IFJAVA8_END
+	void end()
+//IFJAVA8_START
+	{
+	}
+//IFJAVA8_END
+	;
 }

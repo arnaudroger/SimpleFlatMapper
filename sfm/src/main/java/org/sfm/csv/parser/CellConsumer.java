ad524dad04ad5600818649332528d4b72db94a5d
Arnaud Roger <arnaud.roger@gmail.com>
2016-07-11 16:57:38 +0100

    #324 build cell consumer from Consumer<String>

--- sfm/src/main/java/org/sfm/csv/parser/CellConsumer.java
+++ sfm/src/main/java/org/sfm/csv/parser/CellConsumer.java
@@ -1,5 +1,7 @@
 package org.sfm.csv.parser;
 
+import java.util.function.Consumer;
+
 //IFJAVA8_START
 @FunctionalInterface
 //IFJAVA8_END
@@ -23,4 +25,11 @@ public interface CellConsumer {
 	}
 //IFJAVA8_END
 	;
+
+
+//IFJAVA8_START
+	static CellConsumer of(Consumer<String> c) {
+		return ((chars, offset, length) -> c.accept(new String(chars, offset, length)));
+	}
+//IFJAVA8_END
 }

27ed35b329e2e0fde5952448dbabd2ccc368ac68
Arnaud Roger <arnaud.roger@gmail.com>
2016-08-05 18:57:53 +0000

    bundle manifest clean up

--- sfm-jdbc/src/main/java/module-info.java
+++ sfm-jdbc/src/main/java/module-info.java
@@ -3,5 +3,7 @@ module org.simpleflatmapper.jdbc {
     requires java.sql;
     requires javax.persistence;
     requires mysql.connector.java;
+    requires jada.time;
     exports org.simpleflatmapper.jdbc;
+    exports org.simpleflatmapper.jdbc.named;
 }

d4d51bc2a7cf474791298952ed994fd46cc9fb3c
Arnaud Roger <arnaud.roger@gmail.com>
2016-08-16 22:52:40 +0100

    fix java7

--- sfm-map/src/test/java/org/simpleflatmapper/map/mapper/JoinMapperImplTest.java
+++ sfm-map/src/test/java/org/simpleflatmapper/map/mapper/JoinMapperImplTest.java
@@ -18,7 +18,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+//IFJAVA8_START
 import java.util.stream.Collectors;
+//IFJAVA8_END
 
 import static org.junit.Assert.*;
 

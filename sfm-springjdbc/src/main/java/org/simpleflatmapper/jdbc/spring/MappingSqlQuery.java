dbb424c82800b2b94b00c9d67ad7b19237e479df
Arnaud Roger <arnaud.roger@gmail.com>
2016-09-07 17:41:36 +0100

    add test mappingsqlquery and resolve parameter

--- sfm-springjdbc/src/main/java/org/simpleflatmapper/jdbc/spring/MappingSqlQuery.java
+++ sfm-springjdbc/src/main/java/org/simpleflatmapper/jdbc/spring/MappingSqlQuery.java
@@ -2,15 +2,19 @@ package org.simpleflatmapper.jdbc.spring;
 
 import org.simpleflatmapper.jdbc.JdbcMapper;
 import org.simpleflatmapper.jdbc.JdbcMapperFactory;
+import org.simpleflatmapper.jdbc.named.NamedParameter;
+import org.simpleflatmapper.jdbc.named.NamedSqlQuery;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.SqlParameter;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterUtils;
 import org.springframework.jdbc.core.namedparam.ParsedSql;
 import org.springframework.jdbc.object.SqlQuery;
 
 import javax.sql.DataSource;
+import java.sql.SQLType;
 import java.util.List;
 import java.util.Map;
 
@@ -20,7 +24,23 @@ public class MappingSqlQuery<T> extends SqlQuery<T> {
 
     public MappingSqlQuery(DataSource ds, String sql, JdbcMapper<T> mapper) {
         super(ds, sql);
+        setParameters(extractParameters(sql));
         statementCallback = new PreparedStatementCallbackImpl<T>(mapper);
+
+        afterPropertiesSet();
+    }
+
+    private SqlParameter[] extractParameters(String sql) {
+        NamedSqlQuery query = NamedSqlQuery.parse(sql);
+
+        SqlParameter[] params = new SqlParameter[query.getParametersSize()];
+
+        for(int i = 0; i < params.length; i++) {
+            NamedParameter namedParameter = query.getParameter(i);
+            params[i] = new SqlParameter(namedParameter.getName(), 0);
+        }
+
+        return params;
     }
 
     public List<T> execute(Object[] params, Map<?, ?> context) throws DataAccessException {

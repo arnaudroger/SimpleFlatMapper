c44ff0519abe0dc9aaf30735e8930b3db699e602
Arnaud Roger <arnaud.roger@gmail.com>
2016-09-13 21:35:23 +0100

    move comment out of java8 block

--- sfm-util/src/main/java/org/simpleflatmapper/util/RowHandler.java
+++ sfm-util/src/main/java/org/simpleflatmapper/util/RowHandler.java
@@ -5,8 +5,8 @@ package org.simpleflatmapper.util;
  */
 @Deprecated
 public interface RowHandler<T> extends CheckedConsumer<T> {
-	//IFJAVA8_START
 	// bridge method only available for java8
+	//IFJAVA8_START
 	default void handle(T t) throws Exception {
 		accept(t);
 	}

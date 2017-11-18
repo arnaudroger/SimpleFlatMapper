a72da94531ad56c246643b456e0fc50297eb1b1b
Arnaud Roger <arnaud.roger@gmail.com>
2017-11-18 08:05:09 +0000

    export all packages

--- sfm-test/src/main/java/module-info.java
+++ sfm-test/src/main/java/module-info.java
@@ -1,5 +1,7 @@
 module sfm.test {
-        exports org.simpleflatmapper.test;
+        exports org.simpleflatmapper.test.beans;
+        exports org.simpleflatmapper.test.jdbc;
+        exports org.simpleflatmapper.test.junit;
         requires junit;
         requires java.sql;
         requires java.naming;

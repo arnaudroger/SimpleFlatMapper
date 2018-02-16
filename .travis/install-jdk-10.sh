398535e34de1bdc16c890acc46e571be3112d615
Arnaud Roger <arnaud.roger@gmail.com>
2018-02-16 19:55:00 +0000

    fix download link

--- .travis/install-jdk-10.sh
+++ .travis/install-jdk-10.sh
@@ -4,12 +4,12 @@ set -e
 JDK_FEATURE=10
 
 TMP=$(curl -L jdk.java.net/${JDK_FEATURE})
-TMP="${TMP#*Most recent build: jdk-${JDK_FEATURE}-ea+}" # remove everything before the number
+TMP="${TMP#*Most recent build: jdk-${JDK_FEATURE}-+}" # remove everything before the number
 TMP="${TMP%%<*}"                                        # remove everything after the number
 JDK_BUILD="$(echo -e "${TMP}" | tr -d '[:space:]')" # remove all whitespace
 
-JDK_ARCHIVE=jdk-${JDK_FEATURE}-ea+${JDK_BUILD}_linux-x64_bin.tar.gz
-
+JDK_ARCHIVE=jdk-${JDK_FEATURE}-+${JDK_BUILD}_linux-x64_bin.tar.gz
+#https://download.java.net/java/jdk10/archive/43/BCL/jdk-10+43_linux-x64_bin.tar.gz
 cd ~
 wget https://download.java.net/java/jdk${JDK_FEATURE}/archive/${JDK_BUILD}/BCL/${JDK_ARCHIVE}
 tar -xzf ${JDK_ARCHIVE}

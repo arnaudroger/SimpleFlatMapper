7d0892f3db6d9e63071014fc51bde373c7124567
Arnaud Roger <arnaud.roger@gmail.com>
2018-02-28 17:15:20 +0000

    update release

--- release.sh
+++ release.sh
@@ -17,8 +17,8 @@ function java6 {
 #exit
 java8
 rm release.properties
-REL=3.15.9
-DEV=3.15.10-SNAPSHOT
+REL=3.15.10
+DEV=3.15.11-SNAPSHOT
 mvn --batch-mode -Dtag=sfm-parent-$REL -Pdev release:prepare \
                  -DreleaseVersion=$REL \
                  -DdevelopmentVersion=$DEV
@@ -27,7 +27,7 @@ cp release.properties tmp/release.properties
 #GPG_TTY=$(tty)
 #export GPG_TTY
 
-REPOID=orgsimpleflatmapper-1594
+REPOID=orgsimpleflatmapper-1595
 
 java7
 cp tmp/release.properties .

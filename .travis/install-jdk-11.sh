4911288e1abb6e2f7d75c7fffc4b54478646c1f7
Arnaud Roger <arnaud.roger@gmail.com>
2018-03-24 16:22:40 +0000

    fix jdk 11

--- .travis/install-jdk-11.sh
+++ .travis/install-jdk-11.sh
@@ -4,14 +4,13 @@ set -e
 JDK_FEATURE=11
 
 TMP=$(curl -L jdk.java.net/${JDK_FEATURE})
-TMP="${TMP#*Most recent build: jdk-${JDK_FEATURE}+}" # remove everything before the number
+TMP="${TMP#*Most recent build: jdk-${JDK_FEATURE}-ea+}" # remove everything before the number
 TMP="${TMP%%<*}"                                        # remove everything after the number
 JDK_BUILD="$(echo -e "${TMP}" | tr -d '[:space:]')" # remove all whitespace
 
-JDK_ARCHIVE=jdk-${JDK_FEATURE}+${JDK_BUILD}_linux-x64_bin.tar.gz
-
+JDK_ARCHIVE=openjdk-${JDK_FEATURE}-ea+${JDK_BUILD}_linux-x64_bin.tar.gz
 cd ~
-wget https://download.java.net/java/jdk${JDK_FEATURE}/archive/${JDK_BUILD}/BCL/${JDK_ARCHIVE}
+wget https://download.java.net/java/early_access/jdk${JDK_FEATURE}/${JDK_BUILD}/GPL/${JDK_ARCHIVE}
 tar -xzf ${JDK_ARCHIVE}
 export JAVA_HOME=~/jdk-${JDK_FEATURE}
 export PATH=${JAVA_HOME}/bin:$PATH

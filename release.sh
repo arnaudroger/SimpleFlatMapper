becf564606cfb85d0170d5e586630db3e23bc484
Arnaud Roger <arnaud.roger@gmail.com>
2018-04-01 18:13:35 +0100

    fix releasE

--- release.sh
+++ release.sh
@@ -17,8 +17,8 @@ function java6 {
 #exit
 java8
 rm release.properties
-REL=3.16.2
-DEV=3.16.3-SNAPSHOT
+REL=3.16.3
+DEV=3.16.4-SNAPSHOT
 mvn --batch-mode -Dtag=sfm-parent-$REL -Pdev release:prepare \
                  -DreleaseVersion=$REL \
                  -DdevelopmentVersion=$DEV
@@ -27,7 +27,7 @@ cp release.properties tmp/release.properties
 #GPG_TTY=$(tty)
 #export GPG_TTY
 
-REPOID=orgsimpleflatmapper-1599
+REPOID=orgsimpleflatmapper-1600
 
 java7
 cp tmp/release.properties .

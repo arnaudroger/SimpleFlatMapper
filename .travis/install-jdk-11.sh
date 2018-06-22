07e1b1c25cf8fd8e65b48506f4de90f4ffe94b89
Arnaud Roger <arnaud.roger@gmail.com>
2018-06-22 21:46:40 +0100

    jdk 11 install script

--- .travis/install-jdk-11.sh
+++ .travis/install-jdk-11.sh
@@ -4,8 +4,8 @@ set -e
 JDK_FEATURE=11
 
 TMP=$(curl -L jdk.java.net/${JDK_FEATURE})
-TMP="${TMP#*Latest build: jdk-${JDK_FEATURE}-ea+}" # remove everything before the number
-TMP="${TMP%%<*}"                                        # remove everything after the number
+TMP="${TMP#*Latest build: }" # remove everything before the number
+TMP="${TMP%%\(*}"                                        # remove everything after the number
 JDK_BUILD="$(echo -e "${TMP}" | tr -d '[:space:]')" # remove all whitespace
 
 JDK_ARCHIVE=openjdk-${JDK_FEATURE}-ea+${JDK_BUILD}_linux-x64_bin.tar.gz

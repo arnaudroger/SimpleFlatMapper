491d90d11d869967e40b780b01e13c9b45b8e91a
Arnaud Roger <arnaud.roger@gmail.com>
2018-08-13 19:22:43 +0100

    update

--- release.sh
+++ release.sh
@@ -17,9 +17,9 @@ function java6 {
 #exit
 java8
 rm release.properties
-REL=4.0.0-beta
+REL=4.0.0
 DEV=4.0.0-SNAPSHOT
-REPOID=orgsimpleflatmapper-1620
+REPOID=orgsimpleflatmapper-1621
 mvn --batch-mode -Dtag=sfm-parent-$REL -Pdev release:prepare \
                  -DreleaseVersion=$REL \
                  -DdevelopmentVersion=$DEV

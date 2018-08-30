14e60823e21884c5c09b595cd6347f6319b698a4
Arnaud Roger <arnaud.roger@gmail.com>
2018-08-30 21:44:36 +0100

    fix java 11 url

--- .travis/install-jdk-11.sh
+++ .travis/install-jdk-11.sh
@@ -8,7 +8,8 @@ TMP="${TMP#*Latest build: }" # remove everything before the number
 TMP="${TMP%%\(*}"                                        # remove everything after the number
 JDK_BUILD="$(echo -e "${TMP}" | tr -d '[:space:]')" # remove all whitespace
 
-JDK_ARCHIVE=openjdk-${JDK_FEATURE}-ea+${JDK_BUILD}_linux-x64_bin.tar.gz
+JDK_ARCHIVE=openjdk-${JDK_FEATURE}+${JDK_BUILD}_linux-x64_bin.tar.gz
+
 cd ~
 wget https://download.java.net/java/early_access/jdk${JDK_FEATURE}/${JDK_BUILD}/GPL/${JDK_ARCHIVE}
 tar -xzf ${JDK_ARCHIVE}

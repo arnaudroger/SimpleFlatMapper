0ff80982d9b70560d1ec12d17ffb70d478e4fb01
Arnaud Roger <arnaud.roger@gmail.com>
2018-10-02 10:50:35 +0100

    jdk12 build

---
+++ .travis/install-jdk-12.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+JDK_FEATURE=12
+
+TMP=$(curl -L jdk.java.net/${JDK_FEATURE})
+TMP="${TMP#*Latest build: }" # remove everything before the number
+TMP="${TMP%%\(*}"                                        # remove everything after the number
+JDK_BUILD="$(echo -e "${TMP}" | tr -d '[:space:]')" # remove all whitespace
+
+#JDK_ARCHIVE=openjdk-11_linux-x64_bin.tar.gz
+JDK_ARCHIVE=openjdk-${JDK_FEATURE}-ea+${JDK_BUILD}_linux-x64_bin.tar.gz
+
+cd ~
+#wget https://download.java.net/java/ga/jdk11/openjdk-11_linux-x64_bin.tar.gz
+# https://download.java.net/java/early_access/jdk12/13/GPL/openjdk-12-ea+13_linux-x64_bin.tar.gz
+# https://download.java.net/java/early_access/jdk12/13/GPL/openjdk-12-ea+13_linux-x64_bin.tar.gz
+
+wget https://download.java.net/java/early_access/jdk${JDK_FEATURE}/${JDK_BUILD}/GPL/${JDK_ARCHIVE}
+tar -xzf ${JDK_ARCHIVE}
+export JAVA_HOME=~/jdk-${JDK_FEATURE}
+export PATH=${JAVA_HOME}/bin:$PATH
+cd -
+echo check java version
+java --version
+
+wget https://archive.apache.org/dist/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.zip
+unzip -qq apache-maven-3.5.2-bin.zip
+export M2_HOME=$PWD/apache-maven-3.5.2
+export PATH=$M2_HOME/bin:$PATH

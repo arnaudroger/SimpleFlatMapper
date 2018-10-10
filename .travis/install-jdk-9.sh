9111929643801e3a82de32f501d9ee7742976d6d
Arnaud Roger <arnaud.roger@gmail.com>
2018-10-10 12:00:23 +0100

    fix maven version

--- .travis/install-jdk-9.sh
+++ .travis/install-jdk-9.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 sudo apt-get -y install oracle-java9-installer
-wget https://www-eu.apache.org/dist/maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.zip
+wget https://www-eu.apache.org/dist/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.zip
 unzip -qq apache-maven-3.5.2-bin.zip
 export M2_HOME=$PWD/apache-maven-3.5.2
 export PATH=$M2_HOME/bin:$PATH

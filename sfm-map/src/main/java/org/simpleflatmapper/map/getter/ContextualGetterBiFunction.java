3a0e2525ff6782b9f1f1240921f10c53845db12e
Arnaud Roger <arnaud.roger@gmail.com>
2019-01-23 21:28:02 +0000

    attempt to fix #606, also address  #607

--- sfm-map/src/main/java/org/simpleflatmapper/map/getter/ContextualGetterBiFunction.java
+++ sfm-map/src/main/java/org/simpleflatmapper/map/getter/ContextualGetterBiFunction.java
@@ -2,7 +2,6 @@ package org.simpleflatmapper.map.getter;
 
 import org.simpleflatmapper.converter.Context;
 import org.simpleflatmapper.map.MappingContext;
-import org.simpleflatmapper.map.getter.ContextualGetter;
 import org.simpleflatmapper.util.BiFunction;
 import org.simpleflatmapper.util.ErrorHelper;
 
@@ -17,114 +16,42 @@ public class ContextualGetterBiFunction<S, T> implements BiFunction<S, Context, 
         if (type.isPrimitive()) {
             if (boolean.class.equals(type) && getter instanceof BooleanContextualGetter) {
                 final BooleanContextualGetter pGetter = (BooleanContextualGetter) getter;
-                return (BiFunction<? super S, ? super MappingContext<?>, T>) new BiFunction<S, MappingContext<?>, Boolean>() {
-                    @Override
-                    public Boolean apply(S s, MappingContext<?> mappingContext) {
-                        try {
-                            return pGetter.getBoolean(s, mappingContext);
-                        } catch (Exception e) {
-                            return ErrorHelper.rethrow(e);
-                        }
-                    }
-                };
+                return (BiFunction<? super S, ? super MappingContext<?>, T>) new BooleanBiFunction<S>(pGetter);
             }
 
             if (byte.class.equals(type) && getter instanceof ByteContextualGetter) {
                 final ByteContextualGetter pGetter = (ByteContextualGetter) getter;
-                return (BiFunction<? super S, ? super MappingContext<?>, T>) new BiFunction<S, MappingContext<?>, Byte>() {
-                    @Override
-                    public Byte apply(S s, MappingContext<?> mappingContext) {
-                        try {
-                            return pGetter.getByte(s, mappingContext);
-                        } catch (Exception e) {
-                            return ErrorHelper.rethrow(e);
-                        }
-                    }
-                };
+                return (BiFunction<? super S, ? super MappingContext<?>, T>) new ByteBiFunction<S>(pGetter);
             }
 
             if (char.class.equals(type) && getter instanceof CharacterContextualGetter) {
                 final CharacterContextualGetter pGetter = (CharacterContextualGetter) getter;
-                return (BiFunction<? super S, ? super MappingContext<?>, T>) new BiFunction<S, MappingContext<?>, Character>() {
-                    @Override
-                    public Character apply(S s, MappingContext<?> mappingContext) {
-                        try {
-                            return pGetter.getCharacter(s, mappingContext);
-                        } catch (Exception e) {
-                            return ErrorHelper.rethrow(e);
-                        }
-                    }
-                };
+                return (BiFunction<? super S, ? super MappingContext<?>, T>) new CharacterBiFunction<S>(pGetter);
             }
 
             if (short.class.equals(type) && getter instanceof ShortContextualGetter) {
                 final ShortContextualGetter pGetter = (ShortContextualGetter) getter;
-                return (BiFunction<? super S, ? super MappingContext<?>, T>) new BiFunction<S, MappingContext<?>, Short>() {
-                    @Override
-                    public Short apply(S s, MappingContext<?> mappingContext) {
-                        try {
-                            return pGetter.getShort(s, mappingContext);
-                        } catch (Exception e) {
-                            return ErrorHelper.rethrow(e);
-                        }
-                    }
-                };
+                return (BiFunction<? super S, ? super MappingContext<?>, T>) new ShortBiFunction<S>(pGetter);
             }
 
             if (int.class.equals(type) && getter instanceof IntContextualGetter) {
                 final IntContextualGetter pGetter = (IntContextualGetter) getter;
-                return (BiFunction<? super S, ? super MappingContext<?>, T>) new BiFunction<S, MappingContext<?>, Integer>() {
-                    @Override
-                    public Integer apply(S s, MappingContext<?> mappingContext) {
-                        try {
-                            return pGetter.getInt(s, mappingContext);
-                        } catch (Exception e) {
-                            return ErrorHelper.rethrow(e);
-                        }
-                    }
-                };
+                return (BiFunction<? super S, ? super MappingContext<?>, T>) new IntegerBiFunction<S>(pGetter);
             }
 
             if (long.class.equals(type) && getter instanceof LongContextualGetter) {
                 final LongContextualGetter pGetter = (LongContextualGetter) getter;
-                return (BiFunction<? super S, ? super MappingContext<?>, T>) new BiFunction<S, MappingContext<?>, Long>() {
-                    @Override
-                    public Long apply(S s, MappingContext<?> mappingContext) {
-                        try {
-                            return pGetter.getLong(s, mappingContext);
-                        } catch (Exception e) {
-                            return ErrorHelper.rethrow(e);
-                        }
-                    }
-                };
+                return (BiFunction<? super S, ? super MappingContext<?>, T>) new LongBiFunction<S>(pGetter);
             }
 
             if (float.class.equals(type) && getter instanceof FloatContextualGetter) {
                 final FloatContextualGetter pGetter = (FloatContextualGetter) getter;
-                return (BiFunction<? super S, ? super MappingContext<?>, T>) new BiFunction<S, MappingContext<?>, Float>() {
-                    @Override
-                    public Float apply(S s, MappingContext<?> mappingContext) {
-                        try {
-                            return pGetter.getFloat(s, mappingContext);
-                        } catch (Exception e) {
-                            return ErrorHelper.rethrow(e);
-                        }
-                    }
-                };
+                return (BiFunction<? super S, ? super MappingContext<?>, T>) new FloatBiFunction<S>(pGetter);
             }
 
             if (double.class.equals(type) && getter instanceof DoubleContextualGetter) {
                 final DoubleContextualGetter pGetter = (DoubleContextualGetter) getter;
-                return (BiFunction<? super S, ? super MappingContext<?>, T>) new BiFunction<S, MappingContext<?>, Double>() {
-                    @Override
-                    public Double apply(S s, MappingContext<?> mappingContext) {
-                        try {
-                            return pGetter.getDouble(s, mappingContext);
-                        } catch (Exception e) {
-                            return ErrorHelper.rethrow(e);
-                        }
-                    }
-                };
+                return (BiFunction<? super S, ? super MappingContext<?>, T>) new DoubleBiFunction<S>(pGetter);
             }
             
         } 
@@ -138,6 +65,142 @@ public class ContextualGetterBiFunction<S, T> implements BiFunction<S, Context, 
             return fieldMapperGetter.get(s, mappingContext);
         } catch (Exception e) {
             return ErrorHelper.rethrow(e);
+        }
+    }
+
+    public static final class BooleanBiFunction<S> implements BiFunction<S, MappingContext<?>, Boolean> {
+        private final BooleanContextualGetter pGetter;
+
+        public BooleanBiFunction(BooleanContextualGetter pGetter) {
+            this.pGetter = pGetter;
+        }
+
+        @Override
+        public Boolean apply(S s, MappingContext<?> mappingContext) {
+            try {
+                return pGetter.getBoolean(s, mappingContext);
+            } catch (Exception e) {
+                return ErrorHelper.rethrow(e);
+            }
+        }
+    }
+
+    public static final class ByteBiFunction<S> implements BiFunction<S, MappingContext<?>, Byte> {
+        private final ByteContextualGetter pGetter;
+
+        public ByteBiFunction(ByteContextualGetter pGetter) {
+            this.pGetter = pGetter;
+        }
+
+        @Override
+        public Byte apply(S s, MappingContext<?> mappingContext) {
+            try {
+                return pGetter.getByte(s, mappingContext);
+            } catch (Exception e) {
+                return ErrorHelper.rethrow(e);
+            }
+        }
+    }
+
+    public static final class CharacterBiFunction<S> implements BiFunction<S, MappingContext<?>, Character> {
+        private final CharacterContextualGetter pGetter;
+
+        public CharacterBiFunction(CharacterContextualGetter pGetter) {
+            this.pGetter = pGetter;
+        }
+
+        @Override
+        public Character apply(S s, MappingContext<?> mappingContext) {
+            try {
+                return pGetter.getCharacter(s, mappingContext);
+            } catch (Exception e) {
+                return ErrorHelper.rethrow(e);
+            }
+        }
+    }
+
+    public static final class ShortBiFunction<S> implements BiFunction<S, MappingContext<?>, Short> {
+        private final ShortContextualGetter pGetter;
+
+        public ShortBiFunction(ShortContextualGetter pGetter) {
+            this.pGetter = pGetter;
+        }
+
+        @Override
+        public Short apply(S s, MappingContext<?> mappingContext) {
+            try {
+                return pGetter.getShort(s, mappingContext);
+            } catch (Exception e) {
+                return ErrorHelper.rethrow(e);
+            }
+        }
+    }
+
+    public static final class IntegerBiFunction<S> implements BiFunction<S, MappingContext<?>, Integer> {
+        private final IntContextualGetter pGetter;
+
+        public IntegerBiFunction(IntContextualGetter pGetter) {
+            this.pGetter = pGetter;
+        }
+
+        @Override
+        public Integer apply(S s, MappingContext<?> mappingContext) {
+            try {
+                return pGetter.getInt(s, mappingContext);
+            } catch (Exception e) {
+                return ErrorHelper.rethrow(e);
+            }
+        }
+    }
+
+    public static final class LongBiFunction<S> implements BiFunction<S, MappingContext<?>, Long> {
+        private final LongContextualGetter pGetter;
+
+        public LongBiFunction(LongContextualGetter pGetter) {
+            this.pGetter = pGetter;
+        }
+
+        @Override
+        public Long apply(S s, MappingContext<?> mappingContext) {
+            try {
+                return pGetter.getLong(s, mappingContext);
+            } catch (Exception e) {
+                return ErrorHelper.rethrow(e);
+            }
+        }
+    }
+
+    public static final class FloatBiFunction<S> implements BiFunction<S, MappingContext<?>, Float> {
+        private final FloatContextualGetter pGetter;
+
+        public FloatBiFunction(FloatContextualGetter pGetter) {
+            this.pGetter = pGetter;
+        }
+
+        @Override
+        public Float apply(S s, MappingContext<?> mappingContext) {
+            try {
+                return pGetter.getFloat(s, mappingContext);
+            } catch (Exception e) {
+                return ErrorHelper.rethrow(e);
+            }
+        }
+    }
+
+    public static final class DoubleBiFunction<S> implements BiFunction<S, MappingContext<?>, Double> {
+        private final DoubleContextualGetter pGetter;
+
+        public DoubleBiFunction(DoubleContextualGetter pGetter) {
+            this.pGetter = pGetter;
+        }
+
+        @Override
+        public Double apply(S s, MappingContext<?> mappingContext) {
+            try {
+                return pGetter.getDouble(s, mappingContext);
+            } catch (Exception e) {
+                return ErrorHelper.rethrow(e);
+            }
         }
     }
 }

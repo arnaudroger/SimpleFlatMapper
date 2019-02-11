728473ae55848214041160045bcc1a98fccad9b4
Arnaud Roger <arnaud.roger@gmail.com>
2019-02-11 22:15:33 +0000

    java7

--- sfm-reflect/src/test/java/org/simpleflatmapper/reflect/test/meta/FoobarValue.java
+++ sfm-reflect/src/test/java/org/simpleflatmapper/reflect/test/meta/FoobarValue.java
@@ -3,9 +3,6 @@ package org.simpleflatmapper.reflect.test.meta;
 import java.util.UUID;
 
 public interface FoobarValue {
-  static ImmutableFoobarValue.Builder builder() {
-    return ImmutableFoobarValue.builder();
-  }
   int foo();
   String bar();
   UUID crux();

6b07c999dc380d954f76d616166d22d8dd3c9a85
Arnaud Roger <arnaud.roger@gmail.com>
2019-02-11 22:06:40 +0000

    fix

--- sfm-reflect/src/main/java/org/simpleflatmapper/reflect/DefaultReflectionService.java
+++ sfm-reflect/src/main/java/org/simpleflatmapper/reflect/DefaultReflectionService.java
@@ -33,6 +33,7 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Member;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.lang.reflect.Type;
 import java.lang.reflect.WildcardType;
 import java.util.ArrayList;

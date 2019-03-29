87519b9e58f3c8027f27689160987ea1e95d00fc
Arnaud Roger <arnaud.roger@gmail.com>
2019-03-29 16:48:00 +0000

    jdk 13

---
+++ .travis/install-jdk-13.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+JDK_FEATURE=13
+
+TMP=$(curl -L jdk.java.net/${JDK_FEATURE})
+TMP="${TMP#*Latest build: }" # remove everything before the number
+TMP="${TMP%%\(*}"                                        # remove everything after the number
+JDK_BUILD="$(echo -e "${TMP}" | tr -d '[:space:]')" # remove all whitespace
+
+#JDK_ARCHIVE=openjdk-11_linux-x64_bin.tar.gz
+
+cd ~
+#wget https://download.java.net/java/early_access/jdk13/14/GPL/openjdk-13-ea+14_linux-x64_bin.tar.gz
+
+JDK_ARCHIVE=openjdk-${JDK_FEATURE}-ea+${JDK_BUILD}_linux-x64_bin.tar.gz
+wget https://download.java.net/java/early_access/jdk${JDK_FEATURE}/${JDK_BUILD}/GPL/${JDK_ARCHIVE}
+tar -xzf ${JDK_ARCHIVE}
+export JAVA_HOME=~/jdk-${JDK_FEATURE}
+export PATH=${JAVA_HOME}/bin:$PATH
+cd -
+echo check java version
+java --version
+
+wget https://www-eu.apache.org/dist/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.zip
+unzip -qq apache-maven-3.5.4-bin.zip
+export M2_HOME=$PWD/apache-maven-3.5.4
+export PATH=$M2_HOME/bin:$PATH

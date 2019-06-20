dd1719d870ef76da530471f0e36ef3773e515d54
Arnaud Roger <arnaud.roger@gmail.com>
2019-06-20 19:48:42 +0100

    generalised JdbcGetterFactoryProperty

--- sfm-jdbc/src/main/java/org/simpleflatmapper/jdbc/property/JdbcGetterFactoryProperty.java
+++ sfm-jdbc/src/main/java/org/simpleflatmapper/jdbc/property/JdbcGetterFactoryProperty.java
@@ -1,57 +1,16 @@
 package org.simpleflatmapper.jdbc.property;
 
-import org.simpleflatmapper.converter.Context;
-import org.simpleflatmapper.jdbc.JdbcColumnKey;
-import org.simpleflatmapper.map.context.MappingContextFactoryBuilder;
-import org.simpleflatmapper.map.getter.ContextualGetter;
-import org.simpleflatmapper.map.getter.ContextualGetterFactory;
-import org.simpleflatmapper.map.mapper.PropertyMapping;
 import org.simpleflatmapper.map.property.GetterFactoryProperty;
-import org.simpleflatmapper.map.property.SetterFactoryProperty;
-import org.simpleflatmapper.reflect.Getter;
-import org.simpleflatmapper.reflect.Setter;
-import org.simpleflatmapper.reflect.SetterFactory;
-import org.simpleflatmapper.reflect.getter.GetterFactory;
-import org.simpleflatmapper.util.TypeHelper;
-
-import java.lang.reflect.Type;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class JdbcGetterFactoryProperty {
     public static <T> GetterFactoryProperty forType(final Class<T> type, final ResultSetGetter<T> getter) {
-        ContextualGetterFactory<ResultSet, JdbcColumnKey> getterFactory = new ContextualGetterFactory<ResultSet, JdbcColumnKey>() {
-            @Override
-            public <P> ContextualGetter<ResultSet, P> newGetter(Type target, JdbcColumnKey key, MappingContextFactoryBuilder<?, JdbcColumnKey> mappingContextFactoryBuilder, Object... properties) {
-                if (TypeHelper.areEquals(type, target)) {
-                    final int index = key.getIndex();
-                    return (ContextualGetter<ResultSet, P>) new ResultSetGetterAdapter<T>(getter, index);
-                }
-                return null;
-            }
-        };
-
-        return new GetterFactoryProperty(getterFactory);
+        return GetterFactoryProperty.forType(type, getter);
     }
 
-
-    public interface ResultSetGetter<T> {
+    public interface ResultSetGetter<T> extends GetterFactoryProperty.IndexedGetter<ResultSet, T> {
         T get(ResultSet ps, int i) throws SQLException;
     }
 
-    private static class ResultSetGetterAdapter<T> implements ContextualGetter<ResultSet, T> {
-        private final ResultSetGetter<T> getter;
-        private final int index;
-
-        public ResultSetGetterAdapter(ResultSetGetter<T> getter, int index) {
-            this.getter = getter;
-            this.index = index;
-        }
-
-        @Override
-        public T get(ResultSet target, Context context) throws Exception {
-            return getter.get(target, index);
-        }
-    }
 }
--- sfm-map/src/main/java/org/simpleflatmapper/map/property/GetterFactoryProperty.java
+++ sfm-map/src/main/java/org/simpleflatmapper/map/property/GetterFactoryProperty.java
@@ -1,12 +1,18 @@
 package org.simpleflatmapper.map.property;
 
 
+import org.simpleflatmapper.converter.Context;
+import org.simpleflatmapper.map.FieldKey;
+import org.simpleflatmapper.map.context.MappingContextFactoryBuilder;
+import org.simpleflatmapper.map.getter.ContextualGetter;
 import org.simpleflatmapper.map.getter.ContextualGetterFactory;
 import org.simpleflatmapper.map.getter.ContextualGetterFactoryAdapter;
 import org.simpleflatmapper.reflect.getter.GetterFactory;
 import org.simpleflatmapper.util.TypeHelper;
 
 import java.lang.reflect.Type;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class GetterFactoryProperty {
     private final ContextualGetterFactory<?, ?> getterFactory;
@@ -50,5 +56,41 @@ public class GetterFactoryProperty {
     private static Type getSourceType(GetterFactory<?, ?> getterFactory) {
         Type[] types = TypeHelper.getGenericParameterForClass(getterFactory.getClass(), GetterFactory.class);
         return types != null ? types[0] : null;
+    }
+
+
+    public static <T, K extends FieldKey<K>, S> GetterFactoryProperty forType(final Class<T> type, final IndexedGetter<S, T> getter) {
+        ContextualGetterFactory<ResultSet, K> getterFactory = new ContextualGetterFactory<ResultSet, K>() {
+            @Override
+            public <P> ContextualGetter<ResultSet, P> newGetter(Type target, K key, MappingContextFactoryBuilder<?, K> mappingContextFactoryBuilder, Object... properties) {
+                if (TypeHelper.areEquals(type, target)) {
+                    final int index = key.getIndex();
+                    return (ContextualGetter<ResultSet, P>) new IndexedGetterAdapter<S, T>(getter, index);
+                }
+                return null;
+            }
+        };
+
+        return new GetterFactoryProperty(getterFactory);
+    }
+
+
+    public interface IndexedGetter<S, T> {
+        T get(S s, int i) throws Exception;
+    }
+
+    private static class IndexedGetterAdapter<S, T> implements ContextualGetter<S, T> {
+        private final IndexedGetter<S, T> getter;
+        private final int index;
+
+        public IndexedGetterAdapter(IndexedGetter<S, T> getter, int index) {
+            this.getter = getter;
+            this.index = index;
+        }
+
+        @Override
+        public T get(S target, Context context) throws Exception {
+            return getter.get(target, index);
+        }
     }
 }

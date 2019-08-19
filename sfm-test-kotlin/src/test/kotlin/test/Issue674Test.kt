b9bd89f5e4e8096b13d34ddcfa2a89b459e2a15e
Arnaud Roger <arnaud.roger@gmail.com>
2019-08-19 11:08:10 +0100

    test for 674

--- sfm-test-kotlin/src/test/kotlin/test/Issue674Test.kt
+++ sfm-test-kotlin/src/test/kotlin/test/Issue674Test.kt
@@ -1,8 +1,11 @@
 import org.junit.Test
 import org.simpleflatmapper.csv.CsvParser
+import org.simpleflatmapper.jdbc.JdbcMapperFactory
+import org.simpleflatmapper.map.getter.ContextualGetter
 import org.simpleflatmapper.tuple.Tuple2
 import org.simpleflatmapper.util.TypeReference
 import test.CsvLine
+import java.sql.ResultSet
 
 import kotlin.test.assertEquals
 
@@ -11,41 +14,37 @@ class Issue674Test {
     @Test
     fun test() {
 
-        val pair = CsvParser.mapTo(object: TypeReference<Pair<Int, Int>>() {})
-                .iterator("elt0,elt1\n1,2").next()
+        val builder = JdbcMapperFactory.newInstance().useAsm(false)
+                .addKeys("parentId", "childId")
+                .addAlias("parentId", "children.parentId")
+                .addAlias("childId", "children.childId")
+                .addAlias("propFromResultSetA", "children.propA")
+                .addAlias("propFromResultSetB", "children.propB")
+                .newBuilder(ParentEntity::class.java)
 
-        println(pair)
-        
-        assertEquals(1, pair.first)
-        assertEquals(2, pair.second)
-        
+
+        builder.addKey("parentId")
+        builder.addKey("childId")
+        builder.addMapping("propFromResultSetA")
+        builder.addMapping("propFromResultSetB")
 
 
-        val triple = CsvParser.mapTo(object: TypeReference<Triple<Int, Int, Int>>() {})
-                .iterator("elt0,elt1,elt2\n1,2,3").next()
-        
-        println(triple)
+        val mapper = builder.mapper();
 
-        assertEquals(1, triple.first)
-        assertEquals(2, triple.second)
-        assertEquals(3, triple.third)
 
-    }
-    @Test
-    fun test488() {
-        val p = CsvParser.mapTo(object: TypeReference<Tuple2<String, List<Tuple2<Integer, Long>>>>() {})
-                .iterator("elt0,elt1_elt0_elt0,elt1_elt0_elt1\n1,2,3").next()
-        println(p);
-    }
-    
-    @Test
-    fun test538() {
-        val p = CsvParser
-                .dsl()
-                .mapTo(CsvLine::class.java)
-                .iterator("field1,field2\na,b")
-                .next();
-        
-        println(p);
+        println("mapper = ${mapper}")
     }
 }
+
+data class ParentEntity(
+        var parentId : Int,
+        var children: MutableList<ChildEntity>
+)
+
+data class ChildEntity(
+        var parentId: Int,
+        var childId: Int,
+        var propA: String,
+        var propB: String
+)
+

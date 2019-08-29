8f58f97b2bf402d3a43514a26d6f9363f40812fc
Arnaud Roger <arnaud.roger@gmail.com>
2019-08-29 16:00:09 +0100

    -version

--- .travis/install-jdk-13.sh
+++ .travis/install-jdk-13.sh
@@ -20,7 +20,7 @@ export JAVA_HOME=~/jdk-${JDK_FEATURE}
 export PATH=${JAVA_HOME}/bin:$PATH
 cd -
 echo check java version
-java --version
+java -version
 
 wget https://www-eu.apache.org/dist/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.zip
 unzip -qq apache-maven-3.5.4-bin.zip

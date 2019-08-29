96bfe3178727ac655d636b9bf1a217648fdf59ce
Arnaud Roger <arnaud.roger@gmail.com>
2019-08-29 21:18:10 +0100

    release

--- release.sh
+++ release.sh
@@ -63,7 +63,7 @@ function release {
 #exit
 REL=8.0.0
 DEV=8.0.1-SNAPSHOT
-REPOID=orgsimpleflatmapper-1670
+REPOID=orgsimpleflatmapper-1671
 
 release 7 $REL $DEV $REPOID
 release 8 $REL $DEV $REPOID

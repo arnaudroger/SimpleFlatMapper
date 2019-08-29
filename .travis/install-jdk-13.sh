0ce54ae659be8fcd90078ad46c597645b871851a
Arnaud Roger <arnaud.roger@gmail.com>
2019-08-29 15:34:43 +0100

    fix download

--- .travis/install-jdk-13.sh
+++ .travis/install-jdk-13.sh
@@ -3,15 +3,16 @@ set -e
 
 JDK_FEATURE=13
 
-TMP=$(curl -L jdk.java.net/${JDK_FEATURE})
-TMP="${TMP#*Latest build: }" # remove everything before the number
-TMP="${TMP%%\(*}"                                        # remove everything after the number
-JDK_BUILD="$(echo -e "${TMP}" | tr -d '[:space:]')" # remove all whitespace
+#TMP=$(curl -L jdk.java.net/${JDK_FEATURE})
+#TMP="${TMP#*Latest build: }" # remove everything before the number
+#TMP="${TMP%%\(*}"                                        # remove everything after the number
+#JDK_BUILD="$(echo -e "${TMP}" | tr -d '[:space:]')" # remove all whitespace
 
-cd ~
-
-JDK_ARCHIVE=openjdk-${JDK_FEATURE}-ea+${JDK_BUILD}_linux-x64_bin.tar.gz
-wget --no-check-certificate https://download.java.net/java/early_access/jdk${JDK_FEATURE}/${JDK_BUILD}/GPL/${JDK_ARCHIVE}
+#cd ~
+#https://download.java.net/java/GA/jdk13/5b8a42f3905b406298b72d750b6919f6/33/GPL/openjdk-13_linux-x64_bin.tar.gz
+#JDK_ARCHIVE=openjdk-${JDK_FEATURE}-ea+${JDK_BUILD}_linux-x64_bin.tar.gz
+#wget --no-check-certificate https://download.java.net/java/early_access/jdk${JDK_FEATURE}/${JDK_BUILD}/GPL/${JDK_ARCHIVE}
+wget --no-check-certificate https://download.java.net/java/GA/jdk13/5b8a42f3905b406298b72d750b6919f6/33/GPL/openjdk-13_linux-x64_bin.tar.gz
 tar -xzf ${JDK_ARCHIVE}
 export JAVA_HOME=~/jdk-${JDK_FEATURE}
 export PATH=${JAVA_HOME}/bin:$PATH

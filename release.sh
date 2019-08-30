6922491dfaa8b2fe7de1f86e43d654ac993602cb
Arnaud Roger <arnaud.roger@gmail.com>
2019-08-30 06:37:08 +0100

    upadte repo

--- release.sh
+++ release.sh
@@ -64,7 +64,7 @@ function release {
 #exit
 REL=8.0.0
 DEV=8.0.1-SNAPSHOT
-REPOID=orgsimpleflatmapper-1671
+REPOID=orgsimpleflatmapper-1672
 
 release 7 $REL $DEV $REPOID
 release 8 $REL $DEV $REPOID

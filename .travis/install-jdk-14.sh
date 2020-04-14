1be039ad38c443b14c9220dae0d143fcbdfccec9
Arnaud Roger <arnaud.roger@gmail.com>
2020-04-14 13:16:59 +0100

    fix install script

--- .travis/install-jdk-14.sh
+++ .travis/install-jdk-14.sh
@@ -12,8 +12,8 @@ cd ~
 #https://download.java.net/java/early_access/jdk14/7/GPL/openjdk-14-ea+7_linux-x64_bin.tar.gz
 #JDK_ARCHIVE=openjdk-${JDK_FEATURE}-ea+${JDK_BUILD}_linux-x64_bin.tar.gz
 #wget --no-check-certificate https://download.java.net/java/early_access/jdk${JDK_FEATURE}/${JDK_BUILD}/GPL/${JDK_ARCHIVE}
-JDK_ARCHIVE=openjdk-14-ea+11_linux-x64_bin.tar.gz
-wget --no-check-certificate https://download.java.net/java/early_access/jdk14/11/GPL/openjdk-14-ea+11_linux-x64_bin.tar.gz
+JDK_ARCHIVE=openjdk-14+36_linux-x64_bin.tar.gz
+wget --no-check-certificate https://download.java.net/openjdk/jdk14/ri/openjdk-14+36_linux-x64_bin.tar.gz
 tar -xzf ${JDK_ARCHIVE}
 export JAVA_HOME=~/jdk-${JDK_FEATURE}
 export PATH=${JAVA_HOME}/bin:$PATH

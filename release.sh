38d61e5f1fecb7e2ad544dfcf40c8d2281212177
Arnaud Roger <arnaud.roger@gmail.com>
2020-05-10 18:02:31 +0100

    release

--- release.sh
+++ release.sh
@@ -93,9 +93,9 @@ function release {
 
 #echo "change versions"
 #exit
-REL=8.2.2
-DEV=8.2.3-SNAPSHOT
-REPOID=orgsimpleflatmapper-1684
+REL=8.2.3
+DEV=8.2.4-SNAPSHOT
+REPOID=orgsimpleflatmapper-1685
 
 release 7 $REL $DEV $REPOID
 release 8 $REL $DEV $REPOID
